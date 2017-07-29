d6a5fc8eb39b1a65871438868aac72765814af1c
Sarena Tran <sarena2821324@gmail.com>
2017-07-28 21:51:44 -0400

    found largest contour

--- main.py
+++ main.py
@@ -39,46 +39,47 @@ import matplotlib.pyplot as plt
 
 # python function: sorted. sort a list based on criteria
 
-camera = cv2.VideoCapture(1)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
-
 upper = np.array([10, 255, 255])
 lower = np.array([0, 128, 64])
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
-while(True):
-    retval, image = camera.read()
+# while(True):
+# to take an image from the webcam
+useless_frames = 30
+camera = cv2.VideoCapture(0)
 
-    # filtering the video
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    filter_color = cv2.inRange(hsv_image, lower, upper)
-    opening_image = cv2.morphologyEx(filter_color, cv2.MORPH_OPEN, kernel)
-    closing_image = cv2.morphologyEx(opening_image, cv2.MORPH_CLOSE, kernel)
-    final_image = cv2.bitwise_and(image, image, mask = closing_image)
+def get_image():
+    retval, img = camera.read()
+    return img
+print("Hold up homie, I'm taking images rn... Say cheese :)")
+for i in range(useless_frames):
+    temp = get_image()
+image = get_image()
 
-    # contouring
-    im2, contours, hierarchy = cv2.findContours(closing_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+# filtering the video/photo
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+filter_color = cv2.inRange(hsv_image, lower, upper)
+opening_image = cv2.morphologyEx(filter_color, cv2.MORPH_OPEN, kernel)
+closing_image = cv2.morphologyEx(opening_image, cv2.MORPH_CLOSE, kernel)
+filtered_hsv = cv2.bitwise_and(image, image, mask = closing_image) # try finding another
+                                                                   # way to smooth the image more
+# contouring
+im2, contours, hierarchy = cv2.findContours(closing_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+area_bounds = []
+if len(contours) != 0:
     # finding the largest contour
-    max_area = 0
-    max_contour_value = 0
-    if len(contours) != 0:
-        for i in range(0, len(contours)):
-            max_contour_value = i
-            contour_area = cv2.contourArea(contours[i])
-            if contour_area > max_area:
-                max_area = contour_area
-        print(' i value: ', max_contour_value, ' max_area: ', max_area)
+    sorted_contours = sorted(contours, key = lambda contour: cv2.contourArea(contour))
+    area_bounds = sorted_contours[-1]
+    print('area_bounds:', area_bounds)
+final_image = cv2.drawContours(image, area_bounds, -1, (0, 255, 0), 2)
 
-    # just to view what happens throughout the code
-    cv2.imshow('original', image)
-    # cv2.imshow('hsv filer', hsv_image)
-    # cv2.imshow('filter color', filter_color)
-    # cv2.imshow('final image', final_image)
-    cv2.drawContours(final_image, contours, -1, (0, 255, 0))
-    cv2.imshow('contours', final_image)
+# just to view what happens throughout the code
+cv2.imshow('original', image)
+# cv2.imshow('hsv filer', hsv_image)
+# cv2.imshow('filter color', filter_color)
+# cv2.imshow('final image', filtered_hsv)
+cv2.imshow('contours', final_image)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+cv2.waitKey(0)
 cv2.destroyAllWindows()
